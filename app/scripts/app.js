c05860cb30be1a29b77061856cbf3ea760af69d4
tim <timford88@gmail.com>
2015-07-30 19:31:12 +0000

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,20 @@
+(function(document) {
+  'use strict';
+  
+    // Listen for template bound event to know when bindings
+    // have resolved and content has been stamped to the page
+    app.addEventListener('dom-change', function() {
+        
+    });
+
+    window.addEventListener('WebComponentsReady', function() {
+        var app = document.querySelector('#app'),
+            pilotList = document.querySelector('xwsb-pilot-list'),
+            squad = document.querySelector('xwsb-squad');
+            
+        pilotList.addEventListener('xwsb-pilot-select', function(evt) {
+            squad.addPilot(evt.detail.pilot);
+        })
+    });
+
+})(document);
